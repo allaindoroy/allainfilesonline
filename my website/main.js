9e32d8c4e19974ff144695f966bf65ead5ec49c0
allaindoroy <99068728+allaindoroy@users.noreply.github.com>
2022-10-11 08:51:53 +0800

    Add files via upload

---
+++ my website/main.js
@@ -0,0 +1,15 @@
+//alert('Welcome');//
+
+var FName= "Allain";
+var LName= "Doroy";
+let Age= "19";
+const Gender="male";
+
+
+console.log(FName,LName);
+console.log(Age);
+console.log(Gender);
+
+
+ function myFunction() {
+          document.getElementById("form").reset();
